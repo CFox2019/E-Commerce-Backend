72dd5d0e9d6f7428f8d472118cdad04a7da5bd13
Courtney Fox <courtney.fox@me.com>
2021-03-20 18:50:30 -0400

    revised routes and added ondelete cascade for category

--- models/index.js
+++ models/index.js
@@ -6,18 +6,20 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id',
+  foreignKey: 'category_id'
 });
 
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product, {
+  onDelete: 'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
     unique: false,
-    onDelete: 'cascade'
+    onDelete: 'CASCADE'
   },
   as: 'product_tags'
 });
@@ -27,7 +29,7 @@ Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
     unique: false,
-    onDelete: 'cascade'
+    onDelete: 'CASCADE'
   },
   as: 'tag_products'
 });
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -35,35 +35,94 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new tag
-  Tag.create(req.body)
-    .then((tag) => {
-      // if there's tag products, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.productIds?.length) {
-        const tagProductIdArr = req.body.productIds.map((product_id) => {
-          return {
-            product_id,
-            tag_id: tag.id,
-          };
-        });
-        return ProductTag.bulkCreate(tagProductIdArr);
-      }
+  try {
+    const tag = await Tag.create(req.body)
+
+    // if there's tag products, we need to create pairings to bulk create in the ProductTag model
+    if (req.body.productIds?.length) {
+      const tagProductIdArr = req.body.productIds.map((product_id) => {
+        return {
+          product_id,
+          tag_id: tag.id,
+        };
+      });
+      await ProductTag.bulkCreate(tagProductIdArr);
+
+      // re-fetch tag to get updated attributes
+      const tagData = await Tag.findByPk(tag.id, {
+        include: [{ model: Product, through: ProductTag, as: 'tag_products' }],
+      });
+
+      res.status(200).json(tagData)
+    } else {
       // if no product tags, just respond
       res.status(200).json(tag);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
-
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tag = await Tag.update(req.body, { where: { id: req.params.id } });
+
+    if (req.body.productIds?.length) {
+      // find all associated tags from ProductTag
+      const productTags = await ProductTag.findAll({ where: { tag_id: req.params.id } });
+
+      // get list of current product_id
+      const currentTagProductIds = productTags.map(({ product_id }) => product_id);
+
+      // create filtered list of new product_ids
+      const newProductTags = req.body.productIds
+        .filter((product_id) => !currentTagProductIds.includes(product_id))
+        .map((product_id) => {
+          return {
+            product_id,
+            tag_id: req.params.id,
+          };
+        });
+
+      // figure out which ones to remove
+      const productTagsToRemove = productTags
+        .filter(({ product_id }) => !req.body.productIds.includes(product_id))
+        .map(({ id }) => id);
+
+      // run both actions
+      await Promise.all([
+        ProductTag.destroy({ where: { id: productTagsToRemove } }),
+        ProductTag.bulkCreate(newProductTags),
+      ]);
+    }
+
+    // re-fetch tag to get updated attributes
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag, as: 'tag_products' }],
+    });
+
+    res.json(tagData)
+  } catch (err) {
+    // console.log(err);
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const  tagData = await Tag.destroy({ where: { id: req.params.id } });
+    if (!tagData) {
+      res.status(400).json({ message: 'No tag found with that id!' });
+      return;
+    }
+
+    res.status(204).send();
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
